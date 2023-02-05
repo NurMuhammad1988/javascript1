c3eb576182bb9241b5a165ff36ace244458dc9eb
NurMuhammad1988 <nurikyorov88@gmail.com>
2023-02-05 21:37:08 +0500

    javascript 1

---
+++ script.js
@@ -0,0 +1,43 @@
+document.write("ekranga chiqarishni ikkinchi usuli");
+alert('ekranga chiqarishni birinchi usuli');
+console .log('ekranga chiqarishni uchinchi usuli');
+confirm("ekranga chiqarishni to'rtinchi usuli");
+
+
+document.write(5+5);
+
+
+var ism = 'Nurmuhammad';
+document.write(ism)
+
+
+
+var ism = '38724646';
+document.write(ism)
+
+
+
+var ism_familya = 'YorovNurmuhammad';
+document.write(ism_familya)
+
+
+
+var name = 'js is so hard';
+document.write(name)
+
+
+
+var name1 = 'doston';  
+document.write(name1) 
+
+
+var name1 = 'dostonmas';
+document.write(name1)
+
+
+
+// const name2 = 'Nurmuhammad';
+
+// name2 = "Nurmuhammad" hato chiqadi
+
+// document.write(name2)
